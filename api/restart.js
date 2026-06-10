a1547827d590a9f12e912ed7ed8500f5f9e99bf9
SHINING <benjaminndjengshining@gmail.com>
2026-06-10 23:01:29 +0100

    Refactor Pusher initialization in restart.js

--- api/restart.js
+++ api/restart.js
@@ -1,4 +1,4 @@
-const Pusher = require('pusher');
+const { createPusher } = require('./_pusher');
 const { CHANNEL, resetGame, getGame, saveGame } = require('./_store');
 
 module.exports = async (req, res) => {
@@ -7,13 +7,8 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
-  const { PUSHER_APP_ID, PUSHER_KEY, PUSHER_SECRET, PUSHER_CLUSTER } = process.env;
-  if (!PUSHER_APP_ID || !PUSHER_KEY || !PUSHER_SECRET || !PUSHER_CLUSTER)
-    return res.status(500).json({ error: 'Configuration serveur manquante.' });
-
-  const pusher = new Pusher({ appId: PUSHER_APP_ID, key: PUSHER_KEY, secret: PUSHER_SECRET, cluster: PUSHER_CLUSTER, useTLS: true });
-
   try {
+    const pusher = createPusher();
     resetGame();
     const game = getGame();
     game.player0Connected = true;
@@ -24,7 +19,6 @@ module.exports = async (req, res) => {
     await pusher.trigger(CHANNEL, 'state', game);
     return res.status(200).json({ ok: true, game });
   } catch (err) {
-    console.error('Erreur restart:', err);
     return res.status(500).json({ error: err.message });
   }
 };
